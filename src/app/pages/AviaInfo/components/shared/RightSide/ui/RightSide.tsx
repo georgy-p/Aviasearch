639b3b25335c8606cad7b55f4aa95deb9e6af43a
George <81578359+georgy-p@users.noreply.github.com>
2023-10-10 19:30:40 +0300

    move to the widgets directory

--- src/app/pages/AviaInfo/components/shared/RightSide/ui/RightSide.tsx
+++
@@ -1,6 +0,0 @@
-import cls from './RightSide.module.scss';
-import { Box } from '@mui/material';
-
-export const RightSide = () => {
-    return <Box className={cls.rightSide}>4150 ₽</Box>;
-};
---
+++ src/widgets/cardComponents/RightSide/ui/RightSide.tsx
@@ -0,0 +1,5 @@
+import cls from './RightSide.module.scss';
+
+export const RightSide = () => {
+    return <div className={cls.rightSide}>4150 ₽</div>;
+};
