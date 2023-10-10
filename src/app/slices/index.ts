9af75823e4a05b1efa72623a199db99119f5eb5b
George <81578359+georgy-p@users.noreply.github.com>
2023-10-10 19:32:02 +0300

    add flightInfo reducer

--- src/app/slices/index.ts
+++ src/app/slices/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import searchReducer from './searchSlice';
+import flightInfoReducer from 'app/slices/flightInfoSlice';
 
 export const store = configureStore({
     reducer: {
         search: searchReducer,
+        flightInfo: flightInfoReducer,
     },
 });
 
