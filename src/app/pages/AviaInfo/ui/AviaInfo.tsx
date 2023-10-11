c56a0f32431ad6c9b8dea4eedaabf9f2afae468a
George <81578359+georgy-p@users.noreply.github.com>
2023-10-11 13:24:07 +0300

    add renderCard function and <PriceSide />

--- src/app/pages/AviaInfo/ui/AviaInfo.tsx
+++ src/app/pages/AviaInfo/ui/AviaInfo.tsx
@@ -1,5 +1,24 @@
+import cls from './AviaInfo.module.scss';
 import { OWCard } from 'app/pages/AviaInfo/components/OWCard';
+import { RTCard } from 'app/pages/AviaInfo/components/RTCard';
+import { PriceSide } from 'widgets/PriceSide';
+import { useSelector } from 'react-redux';
+import { RootState } from 'app/slices';
 
 export const AviaInfo = () => {
-    return <OWCard />;
+    const price = useSelector((state: RootState) => state.flightInfo.price);
+    const renderedCard = () => {
+        const isBack = useSelector(
+            (state: RootState) => state.search.dates.back,
+        );
+        const currentCard = isBack === null ? <OWCard /> : <RTCard />;
+        return currentCard;
+    };
+
+    return (
+        <div className={cls.AviaInfo}>
+            {renderedCard()}
+            <PriceSide price={price} />
+        </div>
+    );
 };
